18ab49843e10f043611efdd658bfaa2ff7aa09d7
vivz753 <vivz753@gmail.com>
2018-09-25 14:43:20 -0700

    added start script to index.js + .gitignore file

--- Project/server/index.js
+++ Project/server/index.js
@@ -5,5 +5,7 @@ app.get('/', (req, res) =>{
 	res.send({ hi: 'there'});
 });
 
+//injects an environment variable which is set in underlying run time during node 
+//if heroku has a defined PORT use it otherwise use port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
